dfe3d187e975d0b938ac3f726bea7153eb19673f
Grace Shih <vocalise@gmail.com>
2019-12-21 21:01:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Your code here
 let dodger = document.getElementById('dodger');
+let left_beginning_str = dodger.style.left.replace("px", "");
+let left_beginning_num = parseInt(left_beginning_str, 10);
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
@@ -14,7 +16,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left > ) {
     dodger.style.left = `${left + 1}px`;
   }
 }
