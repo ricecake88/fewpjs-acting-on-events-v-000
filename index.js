73677b353eb9cbac5da9c987e56f1c8b9d51d298
Grace Shih <vocalise@gmail.com>
2019-12-21 20:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
